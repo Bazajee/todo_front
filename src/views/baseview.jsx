e9b49517a84ffc61891e0ed6bc614d8193965f7f
thomassales <salesthomas96@gmail.com>
2024-02-24 17:21:21 +0100

    display fetching object

--- src/views/baseview.jsx
+++ src/views/baseview.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import Toolbar from "../components/toolbar";
 
 import { useQuery} from 'react-query';
-
 import { getTaskLists } from "../data";
 
 
@@ -12,17 +11,33 @@ import { getTaskLists } from "../data";
 
 
 export default function Baseview() {
-   const { data, isLoading, error } = useQuery('tasks', getTaskLists)
+   
+   const { data, isLoading, error } = useQuery(
+      'tasks', 
+      async () => {
+         const response = await getTaskLists();
+
+         return response;
+      }
+   )
    console.log(isLoading, data)
 
    return (
+
       <div >
-          <div className="flex w-full">  
+         <div className="flex w-full">  
             <div className="m-4">
                <Toolbar />
             </div>
             <div className="m-4">
                <Outlet />
+            </div>
+            <div>
+                  { 
+                     isLoading? (<p>is isLoading</p>) : ( data.map(obj => <p>{obj.name}</p>)) 
+                  }
+                  
+               
             </div>
          </div>
       </div>
